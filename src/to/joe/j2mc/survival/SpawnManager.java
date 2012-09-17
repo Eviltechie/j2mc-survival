2a9eee5d58fdd8883c2ddec9bad6733957910685
Ivan Panarusky <eviltechie@joe.to>
2012-09-16 23:31:39 -0400

    I seem to have forgotten something important

---
+++ src/to/joe/j2mc/survival/SpawnManager.java
@@ -0,0 +1,88 @@
+package to.joe.j2mc.survival;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public class SpawnManager {
+
+    String players[];
+    Location spawns[];
+    int maxPlayers;
+    J2MC_Survival plugin;
+
+    public SpawnManager(World world, List<String> spawnStrings, J2MC_Survival survival) {
+        this.plugin = survival;
+        spawns = new Location[spawnStrings.size()];
+        players = new String[spawnStrings.size()];
+        for (int x = 0; x < spawnStrings.size(); x++) {
+            String splitString[] = spawnStrings.get(x).split(",");
+            spawns[x] = new Location(world, Double.parseDouble(splitString[0]) + .5, Double.parseDouble(splitString[1]) + 1, Double.parseDouble(splitString[2]) + .5);
+        }
+        Collections.shuffle(Arrays.asList(spawns));
+    }
+
+    public boolean addPlayer(String name) {
+        for (int x = 0; x < players.length; x++) {
+            if (players[x] == null) {
+                players[x] = name;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean addPlayerLate(Player player) {
+        String name = player.getName();
+        for (int x = 0; x < players.length; x++) {
+            if (players[x] == null) {
+                players[x] = name;
+                preparePlayer(player);
+                player.teleport(spawns[x]);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void removePlayer(String name) {
+        for (int x = 0; x < players.length; x++) {
+            if (players[x].equals(name)) {
+                players[x] = null;
+                return;
+            }
+        }
+    }
+    
+    public void spawnPlayers() {
+        for (int x = 0; x < players.length; x++) {
+            if (players[x] == null)
+                continue;
+            Player p = this.plugin.getServer().getPlayer(players[x]);
+            if (p == null)
+                continue;
+            preparePlayer(p);
+            p.teleport(spawns[x]);
+        }
+    }
+    
+    public static void preparePlayer(Player p) {
+        p.setGameMode(GameMode.SURVIVAL);
+        p.setAllowFlight(false);
+        p.getInventory().clear();
+        p.getInventory().setHelmet(null);
+        p.getInventory().setChestplate(null);
+        p.getInventory().setLeggings(null);
+        p.getInventory().setBoots(null);
+        p.getInventory().addItem(new ItemStack(Material.COMPASS));
+        p.setHealth(p.getMaxHealth());
+        p.setFoodLevel(20);
+    }
+}
